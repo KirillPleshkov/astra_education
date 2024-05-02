07c6ba8c5883c49d518b3cf5d2f35b2b883cfb8e
kirill <kirypleshkov@mail.ru>
2024-05-02 10:58:45 +0700

    v 1.20 view discipline teachers

--- user/api/views.py
+++ user/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from curriculum.models import CurriculumDiscipline, CurriculumDisciplineUser
 from user.api.serializers import UserSerializer, TeacherSerializer
 
 user_model = get_user_model()
@@ -27,10 +28,22 @@ class TeacherViewSet(viewsets.ViewSet):
 
     def list(self, request):
         name = request.query_params.get('name')
-        serializer = self.serializer_class(self.queryset.filter(
-            Q(first_name__icontains=name) |
-            Q(last_name__icontains=name)
-        ), many=True)
+
+        if name is not None:
+            serializer = self.serializer_class(self.queryset.filter(
+                Q(first_name__icontains=name) |
+                Q(last_name__icontains=name)
+            ), many=True)
+            return Response(serializer.data)
+
+        curriculum = request.query_params.get('curriculum')
+        discipline = request.query_params.get('discipline')
+
+        curriculum_discipline = CurriculumDiscipline.objects.get(curriculum_id=curriculum, discipline_id=discipline)
+
+        curriculum_discipline_users = CurriculumDisciplineUser.objects.filter(curriculum_discipline=curriculum_discipline)
+        teachers = [curriculum_discipline_user.user for curriculum_discipline_user in curriculum_discipline_users]
+        serializer = self.serializer_class(teachers, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk):
